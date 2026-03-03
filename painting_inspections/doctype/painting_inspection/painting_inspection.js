a4fa4219586b42f57c761df3dea188d4018cd19b
Eros-Aphrodite <lucky.super.star0805@gmail.com>
2026-03-03 22:40:13 +1100

    fix the detail tab dobulecate issue

--- painting_inspections/doctype/painting_inspection/painting_inspection.js
+++ painting_inspections/doctype/painting_inspection/painting_inspection.js
@@ -2,5 +2,50 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Painting Inspection", {
-	refresh(frm) {},
+	refresh(frm) {
+		// Keep all Painting Inspection tabs visible and remove the extra auto
+		// \"Details\" tab that Frappe sometimes inserts.
+		setTimeout(() => {
+			const $wrapper = $(frm.wrapper);
+			const $tabs_list = $wrapper.find(".form-tabs-list");
+
+			if (!$tabs_list.length) {
+				return;
+			}
+
+			// Remove Frappe's auto \"__details\" tab if present.
+			const $autoDetails = $tabs_list
+				.find('.nav-link[data-fieldname=\"__details\"]')
+				.closest(\".nav-item\");
+			if ($autoDetails.length) {
+				const targetId = $autoDetails.find(\".nav-link\").attr(\"href\");
+				$autoDetails.remove();
+				if (targetId) {
+					$wrapper.find(targetId).remove();
+				}
+			}
+
+			// Show every remaining tab header (Details, INTERIOR, EXTERIOR).
+			$tabs_list.find(\".nav-item\").each((_, el) => {
+				const $item = $(el);
+				$item.removeClass(\"hide\").addClass(\"show\");
+			});
+
+			// Ensure each tab pane itself is not force-hidden.
+			$wrapper.find(\".form-tab-content .tab-pane\").each((_, el) => {
+				const $pane = $(el);
+				$pane.removeClass(\"hide\");
+			});
+
+			// Make sure at least one tab is active.
+			const $active_link = $tabs_list.find(\".nav-link.active\");
+			if (!$active_link.length) {
+				const $first = $tabs_list.find(\".nav-link\").first();
+				if ($first.length) {
+					$first.tab(\"show\");
+				}
+			}
+		}, 0);
+	},
 });
+
